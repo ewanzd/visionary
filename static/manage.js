53c04b4c26e8735ad36f6c3eee22fc5302ecf531
ewanzd <ewanzd@gmail.com>
2018-01-15 13:19:13 +0200

    auth for all pages

--- static/manage.js
+++ static/manage.js
@@ -4,10 +4,16 @@ const UPDATE = 'PUT';
 const DELETE = 'DELETE';
 
 function sendRequest(url, httpMethod, data) {
+  var auth = getCookie("auth");
+  if (auth == null || auth == "") {
+      alert("Please authenticate you first: http://127.0.0.1:8000/static/auth.html");
+      return {};
+  }
+
   var request = new XMLHttpRequest();
   request.open(httpMethod, url, false);
-  request.setRequestHeader("Authorization", "Basic " + btoa("admin:password123"));
-  request.setRequestHeader("X-CSRFToken", readCookie("csrftoken"));
+  request.setRequestHeader("Authorization", "Basic " + auth); // btoa("admin:password123"));
+  request.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   if (typeof data !== 'undefined') {
     request.send(data);
@@ -21,13 +27,24 @@ function sendRequest(url, httpMethod, data) {
   return {};
 }
 
-function readCookie(name) {
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
  var nameEQ = encodeURIComponent(name) + "=";
  var ca = document.cookie.split(';');
  for (var i = 0; i < ca.length; i++) {
- var c = ca[i];
- while (c.charAt(0) === ' ') c = c.substring(1, c.length);
- if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+   var c = ca[i];
+   while (c.charAt(0) === ' ') {
+     c = c.substring(1, c.length);
+   }
+   if (c.indexOf(nameEQ) === 0) {
+     return decodeURIComponent(c.substring(nameEQ.length, c.length));
+   }
  }
  return null;
 }
